d73f6025997571052d4aa0bc2b5a6b0773b84970
WHYGH <1441829719@qq.com>
2019-05-16 15:55:05 +0800

    快速插入排序

--- 在一个有序数组中插入一个数/在一个有序数组中插入一个数/源.c
+++ 在一个有序数组中插入一个数/在一个有序数组中插入一个数/源.c
@@ -1,22 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 int main() {
 	int a[8] = { 1,2,3,4,6,7,8 };
-	int b[9];
 	int i = 0;
-	int j = 0;
-	printf("数组a为:\n");
-	for (i = 0; i < 8; ++i) {
-		printf("%3d", a[i]);
-	}
-	printf("\n");
-	int b = 5;
-	for (i = 0; i < 8; ++i) {
-		if (a[i]<b&&a[i + 1]>b) {
-		b[i+1]=
+	int b = 0;
+	printf("请输入您要插入的值:\n");
+	scanf("%d", &b);
+	for (i = 0; i < 10; i++)
+		if (a[i] >b)
+		{
+			for (int j = 10; j > i; j--)
+				a[j] = a[j - 1];
+			a[i] = b;
+			break;
 		}
-	}
-	printf("插入后数组b中的元素为;\n");
+	printf("插入后数组a中的元素为;\n");
 	 for (i = 0; i < 8; ++i) {
 		printf("%3d", a[i]);
 	 }
